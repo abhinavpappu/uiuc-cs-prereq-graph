0505da5bafa2b242ea5ec9f2926f2129c1642b2d
Abhinav Pappu <abhi1pappu9@gmail.com>
2021-04-23 15:05:22 -0500

    Update data for Fall 2021

--- get-data.ts
+++ get-data.ts
@@ -4,14 +4,12 @@ import * as fs from "fs";
 
 const parser = xml2js.Parser();
 
-const url = 'https://courses.illinois.edu/cisapp/explorer/catalog/2021/spring/CS.xml?mode=cascade';
+const url = 'https://courses.illinois.edu/cisapp/explorer/catalog/2021/fall/CS.xml?mode=cascade';
 async function main() {
   const response = await axios.get(url)
   const text: string = response.data;
-  console.log(text.slice(0, 100));
 
   const xml = await parser.parseStringPromise(text);
-  console.log(JSON.stringify(xml, null, 2).slice(0, 1000));
 
   await fs.promises.writeFile('data.json', JSON.stringify(xml, null, 2));
 }
